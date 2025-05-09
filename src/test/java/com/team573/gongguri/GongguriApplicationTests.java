f96fd968c86bae66228571c60c7f60aef9dca4b6
김건우 <rjswjddn@gmail.com>
2025-05-09 12:48:17 +0900

    first commit

---
+++ src/test/java/com/team573/gongguri/GongguriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team573.gongguri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongguriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
