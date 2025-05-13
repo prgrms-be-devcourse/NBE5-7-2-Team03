8ce143e6336f5a24ed29c3f5a614ec365e559bc7
JJY <jjy38836038@gmail.com>
2025-05-14 00:33:21 +0900

    feat: 게시글 작성 뷰 초기 세팅

--- src/main/java/com/team573/gongguri/domain/groupPurchase/controller/view/GroupPurchaseViewController.java
+++ src/main/java/com/team573/gongguri/domain/groupPurchase/controller/view/GroupPurchaseViewController.java
@@ -2,12 +2,24 @@ package com.team573.gongguri.domain.groupPurchase.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/groupPurchase")
 public class GroupPurchaseViewController {
-    @GetMapping("/group-purchase")
-    public String testView() {
+
+    // 공동구매 목록 페이지
+    @GetMapping("")
+    public String showHomePage() {
         return "groupPurchase/group-purchase";
+
     }
-    //groupPurchase/group-purchase
+
+    // 공동구매 글 작성 페이지
+    @GetMapping("/post")
+    public String showPostPage() {
+        return "groupPurchase/group-purchase-post";
+    }
+
+
 }
