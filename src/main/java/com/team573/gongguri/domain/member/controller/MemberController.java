d0fad708f592957ba915964faf85906266a0ee0f
Ji-minhyeok <gmh8257@gmail.com>
2025-05-13 14:09:10 +0900

    refactor: 불필요한 어노테이션 삭제

--- src/main/java/com/team573/gongguri/domain/member/controller/MemberController.java
+++ src/main/java/com/team573/gongguri/domain/member/controller/MemberController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
