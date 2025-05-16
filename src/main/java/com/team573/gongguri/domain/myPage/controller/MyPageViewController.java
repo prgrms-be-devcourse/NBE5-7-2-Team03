0be69efff71573bdbe5b747c5b67e302c3153fc9
Ji-minhyeok <gmh8257@gmail.com>
2025-05-16 11:32:51 +0900

    feat: 마이페이지 메인 뷰 구현

--- src/main/java/com/team573/gongguri/domain/myPage/controller/MyPageViewController.java
+++ src/main/java/com/team573/gongguri/domain/myPage/controller/MyPageViewController.java
@@ -1,7 +1,10 @@
 package com.team573.gongguri.domain.myPage.controller;
 
+import com.team573.gongguri.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,18 +14,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyPageViewController {
 
     @GetMapping("")
-    public String showMyPageForm() {
-        return "/my-page/main";
+    public String showMyPageForm(Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        model.addAttribute("nickname", userDetails.getNickname());
+        return "/myPage/main";
     }
 
     @GetMapping("/profile")
     public String showMyProfileForm() {
-        return "/my-page/profile";
+        return "/myPage/profile";
     }
 
     @GetMapping("/purchase")
     public String showMyPurchaseForm() {
-        return "/my-page/purchase";
+        return "/myPage/purchase";
     }
 
 }
