ffd8175a682710723fd164981e321b3a1e4826db
김건우 <rjswjddn@gmail.com>
2025-05-20 10:25:04 +0900

    refactor: 사용자 검증 email 에서 id로 변경, Mapper 메서드명 변경

--- src/main/java/com/team573/gongguri/domain/chat/mapper/ChatMessageMapper.java
+++ src/main/java/com/team573/gongguri/domain/chat/mapper/ChatMessageMapper.java
@@ -2,14 +2,11 @@ package com.team573.gongguri.domain.chat.mapper;
 
 import com.team573.gongguri.domain.chat.dto.ChatMessageResponseDto;
 import com.team573.gongguri.domain.chat.entity.ChatMessage;
-import com.team573.gongguri.domain.chat.entity.ChatRoom;
-import com.team573.gongguri.domain.chat.entity.ChatRoomParticipation;
-import com.team573.gongguri.domain.member.entity.Member;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ChatMapper {
+public class ChatMessageMapper {
     public static ChatMessage toChatMessage(Long roomId, String nickname, String content) {
         return ChatMessage.builder()
             .roomId(roomId)
@@ -18,19 +15,12 @@ public class ChatMapper {
             .build();
     }
 
-    public static ChatMessageResponseDto toChatMessageResponseDto(ChatMessage chatMessage) {
+    public static ChatMessageResponseDto toDto(ChatMessage chatMessage) {
         return ChatMessageResponseDto.builder()
             .messageId(chatMessage.getId().toHexString())
             .content(chatMessage.getContent())
             .nickname(chatMessage.getNickname())
             .createdAt(chatMessage.getCreatedAt())
-            .build();
-    }
-
-    public static ChatRoomParticipation toChatRoomParticipation(Member member, ChatRoom chatRoom) {
-        return ChatRoomParticipation.builder()
-            .member(member)
-            .chatRoom(chatRoom)
             .build();
     }
 }
